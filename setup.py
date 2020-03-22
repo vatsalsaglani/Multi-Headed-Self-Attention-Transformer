bee09a682e60c575e137f9ab46207004bab841f5
vatsalsaglani <saglanivatsal@gmail.com>
2020-03-22 17:04:18 +0530

    added repo url to setup.py file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
 	name='MultiHeadedTransformer', 
     version = '0.1', 
-    url = '',
+    url = 'https://github.com/vatsalsaglani/Multi-Headed-Self-Attention-Transformer',
     description='A packaged code to train a corpus of text with a Multi Headed Self-Attention transformer to generate text.',
 	author = 'Vatsal Saglani', 
     license = 'MIT', 
